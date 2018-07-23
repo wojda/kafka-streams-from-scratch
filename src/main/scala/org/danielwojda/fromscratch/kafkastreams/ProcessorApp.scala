d02f10a67d8c1c346102dd829d393766cbd3ec77
Daniel Wojda <wojdadan@gmail.com>
2018-07-23 22:53:29 +0100

    Refactor real ProcessorApp code

--- src/main/scala/org/danielwojda/fromscratch/kafkastreams/ProcessorApp.scala
+++ src/main/scala/org/danielwojda/fromscratch/kafkastreams/ProcessorApp.scala
@@ -3,22 +3,26 @@ package org.danielwojda.fromscratch.kafkastreams
 import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 import org.apache.kafka.streams.processor.{Processor, ProcessorContext, ProcessorSupplier}
 import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, Topology}
+
 import scala.collection.JavaConverters._
 
 object ProcessorApp extends App {
 
+  lazy val deserializer = new StringDeserializer()
+  lazy val serializer = new StringSerializer()
+  lazy val processorSupplier: ProcessorSupplier[String, String] = () => new UpperCaseProcessor
+
   def start(kafkaBroker: String): Unit = {
+
     val config = new StreamsConfig(Map(
       StreamsConfig.APPLICATION_ID_CONFIG -> "kafka-streams-processor-api",
       StreamsConfig.BOOTSTRAP_SERVERS_CONFIG -> kafkaBroker
     ).asJava)
 
-    val processorSupplier: ProcessorSupplier[String, String] = () => new UpperCaseProcessor
-
     val topology = new Topology()
-      .addSource("Source", new StringDeserializer(), new StringDeserializer(), "input")
+      .addSource("Source", deserializer, deserializer, "input")
       .addProcessor("Processor", processorSupplier, "Source")
-      .addSink("Sink", "output", new StringSerializer(), new StringSerializer(), "Processor")
+      .addSink("Sink", "output", serializer, serializer, "Processor")
 
     new KafkaStreams(topology, config).start()
   }
